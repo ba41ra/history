1070e698f316a7e4d646e40b5d2a3015ed488c94
ba41ra <egormelyh38@gmail.com>
2025-05-11 17:05:03 +0300

    Update history.py

--- history.py
+++ history.py
@@ -1,19 +1,21 @@
 import pdfplumber
 from gtts import gTTS
-def get_pdf_file():  
-    file_path="history.pdf"
+
+
+def get_pdf_file():
+    file_path = "history.pdf"
     with pdfplumber.PDF(open(file_path, mode='rb')) as pdf_file:
         pages = pdf_file.pages
         pages_pdf_text = ''.join([page.extract_text() for page in pages])
         pdf_file = pages_pdf_text.replace("\n", "")
     return pdf_file
 
-   
+
 def get_audio_file(pdf_file):
     language = "ru"
     phrase = gTTS(text=pdf_file, lang=language, slow=False)
     phrase.save("audio_file.mp3")
-    
+
 
 def main():
     pdf_file = get_pdf_file()
